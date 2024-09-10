f091f1ad1843fec1282e4a621b14dc62b84a6f44
nnodim <nnodimobinna@gmail.com>
2024-09-10 21:16:30 +0100

    mobile responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,8 +12,8 @@ import {
 
 export default function Footer() {
   return (
-    <footer className="font-mono" style={{ backgroundImage: `url(${bgImg})` }}>
-      <div className="flex gap-10 p-10 w-full max-w-6xl mx-auto text-center">
+    <footer className="font-mono" style={{ backgroundImage: `url(${bgImg})`, backgroundSize: "cover" }}>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-0 px-4 lg:p-10 w-full max-w-6xl mx-auto text-center">
         <div className="flex flex-col items-center gap-4">
           <img className="w-10 h-10" src={sms} alt="" />
           <p>Feel free to reach out to us with any questions or inquiries.</p>
@@ -24,16 +24,16 @@ export default function Footer() {
           <p>Visit our office for a personal consultation or meeting.</p>
           <p>123 Main St, Anytown, Nigeria</p>
         </div>
-        <div className="flex flex-col items-center gap-4">
+        <div className="flex flex-col items-center gap-4 col-span-full lg:col-span-1">
           <img className="w-10 h-10" src={callCalling} alt="" />
           <p>We&apos;re available to assist you during business hours.</p>
           <p>(123) 456-7890</p>
         </div>
       </div>
       <div className="p-10 w-full max-w-6xl mx-auto text-center">
-        <div className="flex gap-4 w-full justify-between items-center">
+        <div className="flex flex-col md:flex-row gap-4 w-full justify-between items-center">
           <img src={logo} alt="" />
-          <ul className="flex gap-4">
+          <ul className="flex gap-4 flex-wrap lg:flex-nowrap  items-center justify-center">
             <li>
               <a href="#">About</a>
             </li>
@@ -73,8 +73,8 @@ export default function Footer() {
             </li>
           </ul>
         </div>
-        <hr className="my-10" />
-        <div className="flex gap-4 mx-auto w-fit">
+        <hr className="my-10 border border-[#333333]" />
+        <div className="flex gap-4 mx-auto w-fit flex-wrap lg:flex-nowrap items-center justify-center">
           <p>© 2024 Freed. All rights reserved.</p>
           <ul className="flex gap-4">
             <li>Privacy Policy</li>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,14 +5,14 @@ import { ArrowRight } from "lucide-react";
 
 export const LandingPage = () => {
   return (
-    <main className="flex flex-col gap-48 my-20 font-mono">
-      <section className="flex flex-col mx-auto max-w-7xl items-center justify-center gap-10">
+    <main className="flex flex-col gap-24 lg:gap-48 my-10 lg:my-20 font-mono">
+      <section className="flex flex-col mx-auto max-w-7xl items-center justify-center gap-10 px-4">
         <div className="flex flex-col gap-6 justify-center items-center text-center">
-          <h1 className="text-7xl font-bold leading-[88px] w-full max-w-[988px]">
+          <h1 className="text-5xl lg:text-7xl font-bold leading-[88px] w-full max-w-[988px]">
             Welcome To <span className="text-[#6A0572]">BlockWork</span>, the
             Future Of Freelancing
           </h1>
-          <p className="text-xl w-full max-w-3xl">
+          <p className="text-lg w-full max-w-3xl">
             Join our decentralized platform and experience the benefits of
             blockchain technology in the freelance industry.
           </p>
@@ -23,14 +23,14 @@ export const LandingPage = () => {
         </div>
         <img src={heroImg} alt="" />
       </section>
-      <section className="relative mx-auto max-w-7xl w-full flex justify-between items-center">
+      <section className="relative mx-auto max-w-7xl w-full flex flex-col gap-10 lg:flex-row px-4 justify-between items-center">
         <div className="flex flex-col gap-7 max-w-[750px]">
           <p>About</p>
-          <h2 className="text-5xl font-bold leading-[64px]">
+          <h2 className="text-2xl md:text-4xl lg:text-5xl/[64px] font-bold">
             Experience a Decentralized Marketplace for Transparent and{" "}
             <span className="text-primary">Fair Freelancing</span>
           </h2>
-          <p className="text-xl w-full max-w-3xl">
+          <p className="text-lg lg:text-xl w-full max-w-3xl">
             BlockWork is a decentralized marketplace ensures transparency and
             fairness by eliminating intermediaries and providing a secure
             environment for freelancers and clients to collaborate.
@@ -54,28 +54,28 @@ export const LandingPage = () => {
           <img src={ipad} alt="" />
         </div>
       </section>
-      <section className="relative mx-auto max-w-7xl w-full flex gap-10 flex-row-reverse justify-between items-center">
+      <section className="relative mx-auto max-w-7xl w-full flex flex-col gap-10 lg:flex-row-reverse px-4 justify-between items-center">
         <div className="flex flex-col gap-7 max-w-[750px]">
           <p>Feature</p>
-          <h2 className="text-5xl font-bold leading-[64px]">
+          <h2 className="text-2xl md:text-4xl lg:text-5xl/[64px] font-bold">
             Unlock Global Opportunities with Cryptocurrency Payments on
             BlockWork
           </h2>
-          <p className="text-xl w-full max-w-3xl">
+          <p className="textlg lg:text-xl w-full max-w-3xl">
             BlockWork empowers freelancers worldwide by enabling cryptocurrency
             payments, providing access to the freelance economy without banking
             limitations.
           </p>
           <ul className="flex flex-col gap-4">
-            <li className="flex gap-4 text-xl items-center">
+            <li className="flex gap-4 textlg lg:text-xl items-center">
               <img src={trade} alt="" />
               <span>Seamless Cryptocurrency Transactions</span>
             </li>
-            <li className="flex gap-4 text-xl items-center">
+            <li className="flex gap-4 textlg lg:text-xl items-center">
               <img src={trade} alt="" />
               <span>Borderless Payment Solutions for Freelancers</span>
             </li>
-            <li className="flex gap-4 text-xl items-center">
+            <li className="flex gap-4 textlg lg:text-xl items-center">
               <img src={trade} alt="" />
               <span>Financial Freedom with Cryptocurrency Payments</span>
             </li>
@@ -94,12 +94,12 @@ export const LandingPage = () => {
           <img src={featureImg} alt="" />
         </div>
       </section>
-      <section className="w-full mx-auto max-w-7xl gap-24 flex justify-between">
+      <section className="w-full px-4 mx-auto max-w-7xl gap-10 lg:gap-24 flex flex-col lg:flex-row justify-between">
         <div className="flex flex-col gap-4 justify-between">
-          <h2 className="text-4xl font-bold leading-[50px]">
+          <h2 className="text-2xl text-center lg:text-left md:text-4xl/[50px] font-bold">
             Intuitive Dashboards for Freelancers and Clients
           </h2>
-          <div className="flex gap-2">
+          <div className="flex gap-2 justify-center lg:justify-start">
             <Button className="text-white h-auto px-10 rounded-xl">
               Sign Up
             </Button>
@@ -110,15 +110,15 @@ export const LandingPage = () => {
           </div>
         </div>
         <div className="flex flex-col gap-4 w-full max-w-3xl">
-          <p className="text-xl">
+          <p className="text-lg lg:text-xl">
             Freed provides comprehensive user dashboards that offer insights
             into job statuses, financials, communications, and more. These
             intuitive dashboards enhance user experience and productivity.
           </p>
           <div className="flex gap-10">
             <div className="flex flex-col gap-4">
-              <h6 className="font-bold text-xl">Overview</h6>
-              <p className="text-xl">
+              <h6 className="font-bold text-lg lg:text-xl">Overview</h6>
+              <p className="text-lg lg:text-xl">
                 Get a clear overview of your ongoing projects, earnings, and
                 communication history.
               </p>
@@ -134,27 +134,27 @@ export const LandingPage = () => {
         </div>
       </section>
       <section className="relative">
-        <div className="pb-36 w-full flex gap-10 flex-row-reverse justify-between items-center mx-auto max-w-7xl">
+        <div className="lg:pb-36 p-4 w-full flex flex-col gap-10 lg:flex-row-reverse justify-between items-center mx-auto max-w-7xl">
           <img src={bgImg} alt="" className="w-full absolute bottom-0 -z-10" />
           <div className="flex flex-col gap-7 max-w-[750px]">
-            <h2 className="text-5xl font-bold leading-[64px]">
+            <h2 className="text-2xl md:text-4xl lg:text-5xl/[64px] font-bold">
               Expand Your Skills with Freed&#39;s Learning Opportunities
             </h2>
-            <p className="text-xl w-full max-w-3xl">
+            <p className="textlg lg:text-xl w-full max-w-3xl">
               At Freed, we believe in continuous growth and learning. That&#39;s
               why we offer a range of resources and workshops to help you
               enhance your skills and stay ahead in your freelance career.
             </p>
             <ul className="flex flex-col gap-4">
-              <li className="flex gap-4 text-xl items-center">
+              <li className="flex gap-4 textlg lg:text-xl items-center">
                 <img src={trade} alt="" />
                 <span>Access to Expert-led Workshops</span>
               </li>
-              <li className="flex gap-4 text-xl items-center">
+              <li className="flex gap-4 textlg lg:text-xl items-center">
                 <img src={trade} alt="" />
                 <span>Interactive Learning Resources</span>
               </li>
-              <li className="flex gap-4 text-xl items-center">
+              <li className="flex gap-4 textlg lg:text-xl items-center">
                 <img src={trade} alt="" />
                 <span>Community Forums for Knowledge Sharing</span>
               </li>
@@ -174,36 +174,41 @@ export const LandingPage = () => {
           </div>
         </div>
       </section>
-      <section className="w-full gap-24 flex justify-center items-center max-w-7xl mx-auto">
-        <div className="border-[#8E44AD] border w-full flex items-center gap-8">
-          <div className="flex flex-col gap-4 justify-between pl-4">
+      <section className="w-full gap-24 flex p-10 justify-center items-center max-w-7xl mx-auto">
+        <div className="border-[#8E44AD] border w-full flex flex-col-reverse lg:flex-row items-center md:pt-0 gap-8">
+          <div className="flex flex-col gap-8 justify-between p-4 text-center lg:text-left">
             <h2 className="text-4xl font-semibold leading-[50px]">
               Join the Freed Community Today
             </h2>
             <p className="text-xl">
               Discover a new way to freelance and connect with clients globally.
             </p>
-            <div className="flex gap-2">
+            <div className="flex gap-2 w-fit lg:w-full mx-auto">
               <Button className="text-white">Get Started</Button>
               <Button variant="outline">Learn More</Button>
             </div>
           </div>
-          <div className="w-full max-w-[632px] bg-primary flex justify-center items-center py-20">
+          <div className="w-full lg:max-w-[632px] bg-primary flex justify-center items-center py-10 lg:py-20">
             <img src={logoWhite} alt="" />
           </div>
         </div>
       </section>
-      <section className="bg-primary py-28">
+      <section className="bg-primary px-5 py-14 lg:px-0 lg:py-28">
         <div className="flex flex-col gap-8 items-center w-full max-w-3xl mx-auto text-white">
-          <h2 className="text-5xl font-bold leading-[64px] text-center">
+          <h2 className="text-2xl md:text-4xl lg:text-5xl/[64px] font-bold text-center">
             Stay Informed with Our Newsletter
           </h2>
-          <p className="text-xl">
+          <p className="text-lg lg:text-xl">
             Subscribe to receive updates, industry insights, and valuable tips.
           </p>
-          <form className="flex gap-4 w-full">
-            <Input placeholder="Enter your email address" className="w-full h-[72px] text-xl" />
-            <Button className="text-white bg-[#8E44AD] px-10 text-base h-auto">Subscribe</Button>
+          <form className="flex flex-col lg:flex-row gap-4 w-full">
+            <Input
+              placeholder="Enter your email address"
+              className="w-full h-[72px] text-lg lg:text-xl"
+            />
+            <Button className="text-white bg-[#8E44AD] px-10 text-base h-auto w-fit mx-auto">
+              Subscribe
+            </Button>
           </form>
         </div>
       </section>
